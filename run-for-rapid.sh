70d5073ff823f2ad2c41a3fced971ad40a398411
shimobayashi <akimasa@shimobayashi.name>
2016-01-23 21:10:17 +0900

    no rapid aircon

--- run-for-rapid.sh
+++ run-for-rapid.sh
@@ -1,4 +1,3 @@
 curl $JSONJAR_ROOT > /tmp/jsonjar.json
 cat /tmp/jsonjar.json | ./jsonjar2mackerel.rb | logger -t myha2-jsonjar2mackerel
 cat /tmp/jsonjar.json | ./led.rb              | logger -t myha2-led
-cat /tmp/jsonjar.json | ./aircon.rb           | logger -t myha2-aircon
