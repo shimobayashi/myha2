364cd55141f98f01771cca1ee09fd06eb19c74e3
shimobayashi <akimasashimobayashi@gmail.com>
2016-08-01 09:15:16 +0900

    去年の設定だと暑すぎて起きてしまったので除湿を使うようにして根本的に見直し

--- aircon.rb
+++ aircon.rb
@@ -2,13 +2,14 @@
 
 require 'json'
 
-AIRCON_COOLER_ON_THRESHOLD = 80.9
-AIRCON_COOLER_OFF_THRESHOLD = 79.1
+AIRCON_COOLER_ON_THRESHOLD = 80.0
+AIRCON_COOLER_OFF_THRESHOLD = 78.0
 AIRCON_HEATER_ON_THRESHOLD = 60.0
 AIRCON_HEATER_OFF_THRESHOLD = 65.0
 
 def aircon_cooler_on
-  %x[ curl http://192.168.10.18/messages -d `cat aircon_on_cooler_27.irkit` ]
+  #%x[ curl http://192.168.10.18/messages -d `cat aircon_on_cooler_27.irkit` ]
+  %x[ curl http://192.168.10.18/messages -d `cat aircon_on_dehumidify_28.irkit` ]
   puts "cooloer_on: #{$?}"
   sleep 0.5
   epoch = Time.now.to_i
