ed9581da9c3a0bc1625f645eeb4b15930f934e06
shimobayashi <akimasashimobayashi@gmail.com>
2016-08-12 18:14:33 +0900

    外出時にエアコンをきるようにした

--- aircon.rb
+++ aircon.rb
@@ -35,26 +35,34 @@ end
 settings = JSON.parse(STDIN.read)
 p settings
 
-diff = Time.now.to_i - (settings['sleep'] || '0').to_i
-p diff
-if diff < 120 # 赤外線LEDの向きがあってるか確認するため&エアコン手動でつけっぱだったら同期するためにOFFを送る
-  aircon_off
-else
-  discomfort_index = settings['discomfort_index'].to_f
-
-  # 冷房
-  if (discomfort_index > AIRCON_COOLER_ON_THRESHOLD && settings['home'] != '0' && settings['sleep'] != '0') && settings['aircon_on_cooler_27'] == '0' # 在宅中かつ就寝中
-    aircon_cooler_on
-  elsif (discomfort_index < AIRCON_COOLER_OFF_THRESHOLD || settings['home'] == '0' || settings['sleep'] == '0') && settings['aircon_on_cooler_27'] != '0'
+if settings['home'] != '0' # 在宅中
+  diff = Time.now.to_i - (settings['sleep'] || '0').to_i
+  p diff
+  if diff < 120 # 赤外線LEDの向きがあってるか確認するため&エアコン手動でつけっぱだったら同期するためにOFFを送る
     aircon_off
-  end
+  else
+    discomfort_index = settings['discomfort_index'].to_f
 
-  # 暖房
-  if (discomfort_index < AIRCON_HEATER_ON_THRESHOLD && settings['home'] != '0' && settings['sleep'] != '0') && settings['aircon_on_heater_25'] == '0' # 在宅中かつ就寝中
-    if diff > 7.5 * 60 * 60 # 就寝から7時間以上経過
-      aircon_heater_on
+    # 冷房
+    if (discomfort_index > AIRCON_COOLER_ON_THRESHOLD && settings['sleep'] != '0') && settings['aircon_on_cooler_27'] == '0'
+      aircon_cooler_on
+    elsif (discomfort_index < AIRCON_COOLER_OFF_THRESHOLD || settings['sleep'] == '0') && settings['aircon_on_cooler_27'] != '0'
+      aircon_off
     end
-  elsif (discomfort_index > AIRCON_HEATER_OFF_THRESHOLD || settings['home'] == '0' || settings['sleep'] == '0') && settings['aircon_on_heater_25'] != '0'
+
+    # 暖房
+    if (discomfort_index < AIRCON_HEATER_ON_THRESHOLD && settings['sleep'] != '0') && settings['aircon_on_heater_25'] == '0'
+      if diff > 7.5 * 60 * 60 # 就寝から7時間以上経過
+        aircon_heater_on
+      end
+    elsif (discomfort_index > AIRCON_HEATER_OFF_THRESHOLD || settings['sleep'] == '0') && settings['aircon_on_heater_25'] != '0'
+      aircon_off
+    end
+  end
+else
+  diff = Time.now.to_i - (settings['away'] || '0').to_i
+  p diff
+  if diff < 120 # 家を出たらエアコンを切る
     aircon_off
   end
 end
