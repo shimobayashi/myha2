85ba7b81c3eb6831152331904c547fc9a2e6db9d
shimobayashi <akimasashimobayashi@gmail.com>
2017-07-01 20:46:52 +0900

    Mi-lightからHueに乗り換えたので、とりあえず雑に移植

--- led.rb
+++ led.rb
@@ -10,9 +10,8 @@ p settings
 exit if settings['manual_led'] == '1' # 手動運転中
 
 cmd = nil
-cmd2 = nil
 if settings['home'] == '0' # 外出中
-  cmd = "\\x41\\x00\\x55" # 消灯
+  cmd = {"on":false} # 消灯
 else # 在宅中
   if settings['sleep'] == '0' # 寝てない
     t = Time.now
@@ -22,21 +21,19 @@ else # 在宅中
     diff = Time.now.to_i - (settings['force_light'] || '0').to_i
     p diff
     if (diff < 60 * 20)
-      cmd = "\\x42\\x00\\x55" # 点灯
-      cmd2 = "\\xC2\\x00\\x55" # 白色
+      cmd = {"on":true, "bri":254, "ct":153} # 点灯、寒色、明るい
     else
       if (settings['lux'] || '0').to_i > 3000 # 日中でカーテンを両方開けていれば電気をつけない
-        cmd = "\\x41\\x00\\x55" # 消灯
+        cmd = {"on":false} # 消灯
       elsif (settings['lux'] || '0').to_i <= 2500 # 日中でカーテン片方閉じたくらいの暗さであれば電気をつける
-        cmd = "\\x42\\x00\\x55" # 点灯
         if settings['time_to_sleep'] != '0' # 寝る時間
-          cmd2 = "\\x40\\xa0\\x55" # 赤色
-        #elsif (t.hour >= 23 || t.hour < 5) # 23時～5時
-        #  cmd2 = "\\x40\\x95\\x55" # 赤っぽい暖色
-        #elsif t.hour >= 21 # 21時～23時
-        #  cmd2 = "\\x40\\x90\\x55" # 暖色
+          cmd = {"on":true, "bri":196, "ct":500} # 点灯、暖色、ちょっと暗い
+        elsif (t.hour >= 23 || t.hour < 5) # 23時～5時
+          cmd = {"on":true, "bri":254, "ct":500} # 点灯、暖色、明るい
+        elsif t.hour >= 21 # 21時～23時
+          cmd = {"on":true, "bri":254, "ct":250} # 点灯、寒暖色、明るい
         else
-          cmd2 = "\\xC2\\x00\\x55" # 白色
+          cmd = {"on":true, "bri":254, "ct":153} # 点灯、寒色、明るい
         end
       end
     end
@@ -47,22 +44,16 @@ else # 在宅中
     diff = Time.now.to_i - smart_period
     p diff
     if (diff > 0) && (diff < 60 * 60) # スマートウェイクアップ開始後で寝ていれば灯火する。1時間以上経過していたら多分次の日の睡眠とかなので灯火しない
-      cmd = "\\x42\\x00\\x55" # 点灯
-      cmd2 = "\\x40\\xa0\\x55" # 赤色
+      cmd = {"on":true, "bri":64, "ct":500} # 点灯、暖色、かなり暗い
     else
-      cmd = "\\x41\\x00\\x55" # 消灯
+      cmd = {"on":false} # 消灯
     end
   end
 end
 
-p cmd
 if cmd
-  p `bash -c 'echo -ne "#{cmd}" > /dev/udp/192.168.10.16/8899'`
-  p $?
-end
-p cmd2
-if cmd2
-  sleep 0.1
-  p `bash -c 'echo -ne "#{cmd2}" > /dev/udp/192.168.10.16/8899'`
+  cmd = cmd.to_json.gsub(/"/, '\\"')
+  p cmd
+  p `curl -XPUT http://192.168.10.21/api/#{ENV['HUE_USERNAME']}/lights/1/state -d "#{cmd}"`
   p $?
 end
